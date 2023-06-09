ea78e4b475554728a46709ca8c3b7631e1941d05
Jason Prime <600613.m6@gmail.com>
2023-06-09 21:36:27 +0200

    test srv

--- srv.py
+++ srv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
+import socket
 import socketserver
 
 # Configuration
@@ -10,9 +11,12 @@ PORT = 80
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
+
+# Check if any handlers are already added to the logger
+if not logger.handlers:
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
 
 
 class CmdHttpHandler(socketserver.BaseRequestHandler):
@@ -63,14 +67,30 @@ def main():
     # Initialize logging
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+    # Get the IP address where the server is running
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+
+    logger.info("Server is running on IP: %s", ip_address)
     logger.info("To terminate the connection, please enter 'EXIT'.")
     logger.info("Please be aware that certain commands may result in temporary system delays. If this occurs, please retry the command.")
     logger.info("")
 
+    active_connections = set()
+
     try:
         # Start the server
         with socketserver.TCPServer(("0.0.0.0", PORT), CmdHttpHandler) as server:
+            logger.info("Waiting for incoming connections...")
             server.serve_forever()
+
+            # Keep track of active connections
+            while True:
+                if server.active_requests:
+                    for request in server.active_requests:
+                        active_connections.add(request.getpeername())
+                logger.info("Active connections: %s", active_connections)
+
     except Exception as e:
         logger.error(f"Server error occurred: {e}")
 
